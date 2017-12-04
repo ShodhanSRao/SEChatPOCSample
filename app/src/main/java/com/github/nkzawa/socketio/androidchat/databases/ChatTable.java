242ea47815cf95c2895d2a2d3906354660d9adb0
Shodhan <shodhans01@gmail.com>
2017-12-04 15:23:53 +0530

    Test Commit

---
+++ app/src/main/java/com/github/nkzawa/socketio/androidchat/databases/ChatTable.java
@@ -0,0 +1,20 @@
+package com.github.nkzawa.socketio.androidchat.databases;
+
+/**
+ * Created by SESA481157 on 11/23/2017.
+ */
+
+public class ChatTable {
+
+    public static final  String TABLE_NAME = "chats_table";
+
+    public static final  String RECIEVER_NAME ="reciever_name";
+    public static final  String MESSAGE_TYPE = "message_type";
+    public static final  String MESSAGE = "message";
+    public static final  String IS_SENT = "is_sent";
+
+
+
+
+
+}
